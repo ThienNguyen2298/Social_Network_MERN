a75a2a3195b610b811cebe7972e00bcaf1ce6ca2
huuThien <thiennguyen.it.ute@gmail.com>
2020-10-13 01:06:43 +0700

    commit 13-10-2020

---
+++ src/views/Home/Header/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./index.scss";
+
+const Header: React.FC = () => {
+    return <div className="text-danger text-center">
+        Social Network App
+    </div>
+}
+
+export default Header;
